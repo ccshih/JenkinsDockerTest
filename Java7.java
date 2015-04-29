f3e210adeca31db602e420bf1563b7e52c3a9e5a
ccshih <ccshih@localhost.localdomain>
2015-04-29 11:21:10 +0800

    add Java7.java, remove test.txt

---
+++ Java7.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Java7 {
+
+    public static void main(String... args) {
+        Map<String, String> map = new HashMap<>(); //<> operator is supported by Java 7, but no former versions
+        System.out.println(map.size());
+    }
+}
