ec1a1f5acfd96d514c79c9de6b1b59b9e121af84
ccshih <ccshih@localhost.localdomain>
2015-04-29 11:24:20 +0800

    modify comments

--- Java7.java
+++ Java7.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Java7 {
 
     public static void main(String... args) {
-        Map<String, String> map = new HashMap<>(); //<> operator is supported by Java 7, but no former versions
+        Map<String, String> map = new HashMap<>(); //<> operator is supported since Java 7
         System.out.println(map.size());
     }
 }
