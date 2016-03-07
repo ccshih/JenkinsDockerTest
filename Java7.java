86df822f5148ee594e402aeee27acc56e0caf897
ccshih <chiachun.shih@gmail.com>
2016-03-07 20:58:48 +0800

    add an output

--- Java7.java
+++ Java7.java
@@ -5,5 +5,6 @@ class Java7 {
     public static void main(String... args) {
         Map<String, String> map = new HashMap<>(); //<> operator is supported since Java 7
         System.out.println(map.size());
+		System.out.println("test");
     }
 }
